08e4d3ae5f48be6046c7dc7086a986f5a09d3bdb
WassimMensi <wassim.mensi@ecole-isitech.fr>
2023-12-04 13:40:20 +0100

    test8

---
+++ Exercice 1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+private static int Sum(IEnumerable<object> values){ 
+    var sum = 0;
+    foreach(var item in values){
+        switch(item){
+            case 0; 
+            break;
+            case int val;
+            sum += val;
+            break;
+            case IEnumerable<object> subList when subList.Any();
+            sum += Sum(subList)
+        }
+    }
+    return sum;
+}
